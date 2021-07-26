fac605cebe8746901f0f8e40f358f374adb1a705
eduuh <edwinkamaumuraya0@outlook.com>
2021-07-26 07:00:37 +0000

    updated firabase deplo

--- netflix-app/src/firebase.js
+++ netflix-app/src/firebase.js
@@ -1,18 +1,17 @@
-import firebase from "firebase";
-
+import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCbP9zY4u8uYaNohZmp-WSWOSM6c3x4cVQ",
-    authDomain: "netflix-clone-2d498.firebaseapp.com",
-    projectId: "netflix-clone-2d498",
-    storageBucket: "netflix-clone-2d498.appspot.com",
-    messagingSenderId: "358457763579",
-    appId: "1:358457763579:web:c5bc42c29e28f31ab4f25e"
-  };
+  apiKey: 'AIzaSyCbP9zY4u8uYaNohZmp-WSWOSM6c3x4cVQ',
+  authDomain: 'netflix-clone-2d498.firebaseapp.com',
+  projectId: 'netflix-clone-2d498',
+  storageBucket: 'netflix-clone-2d498.appspot.com',
+  messagingSenderId: '358457763579',
+  appId: '1:358457763579:web:c5bc42c29e28f31ab4f25e',
+};
 
-const firebaseApp = firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export { auth }
-export default db
+export { auth };
+export default db;
